387e0efdacfbb234fdd36ed562577351d464c900
Jake Rothenberg <jrothenberg125@gmail.com>
2019-11-26 17:05:57 -0500

    with input changing state

--- screens/RunForm.js
+++ screens/RunForm.js
@@ -1,56 +1,26 @@
-// import React from 'react';
-// import { View, TextInput, ScrollView, StyleSheet, Text } from 'react-native';
-// import {
-//   FormLabel,
-//   FormInput,
-//   FormValidationMessage,
-// } from 'react-native-elements';
-
-// export default class RunForm extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//         address: '',
-//         startTime: '',
-//         endTime: ''
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <ScrollView>
-//         <Text>Post your run below</Text>
-//         <TextInput
-//             value={this.state.address}
-//               placeholder={'Something'}
-//               style={styles.input}
-//             />
-
-//       </ScrollView>
-//     );
-//   }
-// }
-
 import React, { Component } from 'react';
-import { Button, View, ScrollView } from 'react-native';
+import { Button, View, ScrollView, StyleSheet, TextInput } from 'react-native';
 import DateTimePicker from 'react-native-modal-datetime-picker';
 import { Text } from 'react-native-elements';
-import { TextInput } from 'react-native-gesture-handler';
 import TimePicker from 'react-native-simple-time-picker';
+
+import GooglePlacesInput from '../components/GooglePlacesInput'
 
 export default class DateTimePickerTester extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      address: '',
       isDateTimePickerVisible: false,
-      isTimePickerVisible: false,
+      isStartTimePickerVisible: false,
       isEndTimePickerVisible: false,
       date: new Date(),
       startTime: new Date(),
-      endTime: new Date()
+      endTime: new Date(),
     };
   }
 
+  //DATE PICKER
   showDateTimePicker = () => {
     this.setState({ isDateTimePickerVisible: true });
   };
@@ -65,20 +35,22 @@ export default class DateTimePickerTester extends Component {
     this.hideDateTimePicker();
   };
 
-  showTimePicker = () => {
-    this.setState({ isTimePickerVisible: true });
+  //START TIME PICKER
+  showStartTimePicker = () => {
+    this.setState({ isStartTimePickerVisible: true });
   };
 
-  hideTimePicker = () => {
-    this.setState({ isTimePickerVisible: false });
+  hideStartTimePicker = () => {
+    this.setState({ isStartTimePickerVisible: false });
   };
 
-  handleTimePicked = time => {
-    console.log('A date has been picked: ', date);
+  handleStartTimePicked = startTime => {
+    startTime = startTime.getHours();
     this.setState({ startTime });
-    this.hideTimePicker();
+    this.hideStartTimePicker();
   };
 
+  //END TIME PICKER
   showEndTimePicker = () => {
     this.setState({ isEndTimePickerVisible: true });
   };
@@ -88,73 +60,92 @@ export default class DateTimePickerTester extends Component {
   };
 
   handleEndTimePicked = endTime => {
-    console.log('A date has been picked: ', date);
+    endTime = endTime.getHours();
     this.setState({ endTime });
     this.hideTimePicker();
   };
+
+  handleAddressChange(address) {
+    this.setState({ address });
+    console.log(this.state)
+  }
 
   render() {
     return (
       <ScrollView>
         <View>
-          <Button title="Choose a date" onPress={this.showDateTimePicker} />
-          <DateTimePicker
+          <Text style={styles.header}>Create a run for others to see</Text>
+          <View>
+            <Text style={styles.text}>Where would you like to start?</Text>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+              onChangeText={address => this.handleAddressChange(address)}
+              value={this.state.address}
+            />
+          </View>
+          <View>
+            <Button
+              title="Choose a date"
+              onPress={this.showDateTimePicker}
+              style={styles.button}
+            />
+            <DateTimePicker
+              isVisible={this.state.isDateTimePickerVisible}
+              onConfirm={this.handleDatePicked}
+              onCancel={this.hideDateTimePicker}
+              minimumDate={new Date()}
+            />
+          </View>
+            <Button
+              title="Start Time"
+              onPress={this.showStartTimePicker}
+              style={styles.button}
+            />
+            <DateTimePicker
+              mode={'time'}
+              isVisible={this.state.isStartTimePickerVisible}
+              onConfirm={this.handleStartTimePicked}
+              onCancel={this.hideStartTimePicker}
+            />
 
-            isVisible={this.state.isDateTimePickerVisible}
-            onConfirm={this.handleDatePicked}
-            onCancel={this.hideDateTimePicker}
-            minimumDate={new Date()}
-          />
-        </View>
-        <View>
-          <Button title="Start Time" onPress={this.showTimePicker} />
-          <DateTimePicker
-            mode={'time'}
-            isVisible={this.state.isTimePickerVisible}
-            onConfirm={this.handleTimePicked}
-            onCancel={this.hideTimePicker}
-          />
-        </View>
-        <View>
-        <Button title="End Time" onPress={this.showTimePicker} />
-          <DateTimePicker
-            mode={'time'}
-            isVisible={this.state.isEndTimePickerVisible}
-            onConfirm={this.handleEndTimePicked}
-            onCancel={this.hideEndTimePicker}
-          />
-        </View>
 
+            <Button
+              title="End Time"
+              onPress={this.showEndTimePicker}
+              style={styles.button}
+            />
+            <DateTimePicker
+              mode={'time'}
+              isVisible={this.state.isEndTimePickerVisible}
+              onConfirm={this.handleEndTimePicked}
+              onCancel={this.hideEndTimePicker}
+            />
+
+          <Button
+          title='Submit'
+
+          />
+        </View>
       </ScrollView>
     );
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     paddingTop: 100,
-//   },
-//   input: {
-//     width: 200,
-//     height: 44,
-//     marginTop: 10,
-//     padding: 10,
-//     borderWidth: 1,
-//     borderColor: 'black',
-//     borderRadius: 5,
-//     marginBottom: 10,
-//   },
-//   welcomeImage: {
-//     width: 100,
-//     height: 80,
-//     resizeMode: 'contain',
-//     marginTop: 3,
-//     marginLeft: -10,
-//   },
-//   error: {
-//     color: `#eb4034`,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  header: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    borderBottomEndRadius: 30,
+    height: 100,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 15,
+    height: 50,
+  },
+});
