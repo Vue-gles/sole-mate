0a9cc5058ba414e6f8a7c234d62df8ab769e5ee9
Jake Rothenberg <jrothenberg125@gmail.com>
2019-11-29 11:01:06 -0500

    seed now updates with lat and long coords

--- server/db/models/run.js
+++ server/db/models/run.js
@@ -2,65 +2,84 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const { Op } = require('sequelize');
 const User = require('./user');
+const axios = require('axios');
+require('../../../keys');
 
-const Run = db.define('run', {
-  street: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    defaultValue: 'Prospect Park, Brooklyn, NY',
-    validate: {
-      notEmpty: true,
+const Run = db.define(
+  'run',
+  {
+    street: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: 'Prospect Park, Brooklyn, NY',
+      validate: {
+        notEmpty: true,
+      },
+    },
+    city: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: 'Brooklyn',
+      validate: {
+        notEmpty: true,
+      },
+    },
+    state: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: 'NY',
+      validate: {
+        notEmpty: true,
+      },
+    },
+    lat: {
+      type: Sequelize.FLOAT,
+    },
+    long: {
+      type: Sequelize.FLOAT,
+    },
+    startTimeframe: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+    endTimeframe: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+    partnerId: {
+      type: Sequelize.INTEGER,
+    },
+    route: {
+      type: Sequelize.ARRAY(Sequelize.FLOAT),
+    },
+    prefferedMileage: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
   },
-  city: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    defaultValue: 'Brooklyn',
-    validate: {
-      notEmpty: true
-    }
-  },
-  state: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    defaultValue: 'NY',
-    validate: {
-      notEmpty: true
-    }
-  },
-  lat: {
-    type: Sequelize.FLOAT
-  },
-  long: {
-    type: Sequelize.FLOAT
-  },
-  startTimeframe: {
-    type: Sequelize.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.NOW,
-  },
-  endTimeframe: {
-    type: Sequelize.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.NOW,
-  },
-  partnerId: {
-    type: Sequelize.INTEGER,
-  },
-  route: {
-    type: Sequelize.ARRAY(Sequelize.FLOAT)
-  }, 
-  prefferedMileage: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    }
+  {
+    hooks: {
+      //this doesn't work yet
+      beforeValidate: async function(run) {
+        if (!run.lat || !run.long) {
+          const fullAddress = `${run.street}, ${run.city}, ${run.state}`;
+          const {data} = await axios.get(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${fullAddress}&key=${process.env.GOOGLE_API_KEY}`
+          );
+          const lat = data.results[0].geometry.location.lat;
+          const long = data.results[0].geometry.location.lng;
+          run.lat = lat;
+          run.long = long;
+        }
+      },
+    },
   }
-});
-
-
-module.exports = Run;
+);
 
 Run.getPotentialRuns = function(userId) {
   const currentTime = new Date();
@@ -112,3 +131,5 @@ Run.getPastRuns = function(userId) {
   });
   return runs;
 };
+
+module.exports = Run;
