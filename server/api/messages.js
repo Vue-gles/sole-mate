65c36e3e9304bb4b4a0f994e32be929784e2df8e
Megan Donnelly <md2942@nyu.edu>
2019-11-29 14:18:17 -0500

    GET /api/messages

--- server/api/messages.js
+++ server/api/messages.js
@@ -1,27 +1,17 @@
 const router = require('express').Router();
-const { Request, Run, User ,Message} = require('../db');
+const { Request, Run, User, Message } = require('../db');
 const { isAdmin, isUser } = require('../../utils');
-const Op=require('sequelize').Op
-module.exports=router
+const Op = require('sequelize').Op;
+module.exports = router;
 
-router.get('/',isUser,async(req,res,next)=>{
-    try{
-
-        const messages= await Message.findAll({
-            where:{
-                [Op.or]: [
-                    {
-                      senderId: req.user.id,
-                    },
-                    {
-                      receiverId: req.user.id,
-                    },
-                  ],
-            }
-        })
-        console.log(messages)
-        res.json(messages)
-    }catch(err){
-        next(err)
-    }
-})
+// GET /api/messages
+// Returns all messages threads for a user
+router.get('/', isUser, async (req, res, next) => {
+  try {
+    const messages = await Message.getAllMessages(req.user.id);
+    const threads = await Message.getAllThreads(messages, req.user.id);
+    res.send(threads);
+  } catch (err) {
+    next(err);
+  }
+});
