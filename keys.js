70e4a1249680531bd3ab424481e5b277280d8809
Matthew Chen <mc.2green@gmail.com>
2019-12-05 18:11:46 -0500

    post-pull

---
+++ keys.js
@@ -0,0 +1,4 @@
+const GoogleAPIKey = 'AIzaSyDpD7b0_RCWgJ7qhNZCnMdiyBsg1ViBYs0'
+
+module.exports = GoogleAPIKey;
+process.env.GOOGLE_API_KEY = 'AIzaSyDpD7b0_RCWgJ7qhNZCnMdiyBsg1ViBYs0'
---
+++ secrets.js
@@ -0,0 +1,2 @@
+process.env.BACKEND_HOST = `http://172.16.22.117:8080`;
+
