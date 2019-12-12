ca61dcf4fe1606e084646b474e464fbbf1daee5a
Jake Rothenberg <jrothenberg125@gmail.com>
2019-12-12 12:52:41 -0500

    some console log deletes

--- client-expo/client/screens/HomeScreen.js
+++ client-expo/client/screens/HomeScreen.js
@@ -32,24 +32,16 @@ class HomeScreen extends Component {
 
   componentDidMount() {
     registerForPushNotificationsAsync();
-
-    // Handle notifications that are received or selected while the app
-    // is open. If the app was closed and then opened by tapping the
-    // notification (rather than just tapping the app icon to open it),
-    // this function will fire on the next tick after the app starts
-    // with the notification data.
     this._notificationSubscription = Notifications.addListener(
       this._handleNotification
     );
   }
 
   _handleNotification = notification => {
-    // this.setState({ notification: notification });
     alert(notification.data.title);
   };
 
   render() {
-    console.log('HomeScreen View ------------------->');
     return (
       <View style={styles.container}>
         <ScrollView
